aba6897cb76ba7981d295a6eab4804c78c33182a
pcodesdev <njugunapm23@gmail.com>
2023-08-15 10:30:35 +0300

    have finished learning about Kotlin basic datatypes

--- variable/src/main/kotlin/variables.kt
+++ variable/src/main/kotlin/variables.kt
@@ -15,12 +15,52 @@
 //}
 
 //Reassigning variables
-fun main(){
-//    val is readonly
-//    val cartTotal=0
-//    var is mutable
-    var cartTotal=0
-    cartTotal=20
+//fun main(){
+////    val is readonly
+////    val cartTotal=0
+////    var is mutable
+//    var cartTotal=0
+//    println(cartTotal)
+//
+//    cartTotal=20
+//    println(cartTotal)
+//
+////    Increment Operator
+////    cartTotal +=2
+//    cartTotal ++
+//    println(cartTotal)
+//
+////    Decrement Operator
+//    cartTotal --
+//    println(cartTotal)
+//}
 
-    println(cartTotal)
+//Other Data Types
+//01 Double
+//fun main() {
+//    val trip1  = 3.20
+//    val trip2 = 4.10
+//    val trip3 = 1.72
+//    val totalTripLength = trip1 + trip2 + trip3
+//    println("$totalTripLength miles left to destination")
+//}
+
+//02 String
+//fun main() {
+//    val nextMeeting = "Next meeting: "
+//    val date = "January 1 "
+//    val place = "at work "
+//    val reminder = nextMeeting + date + place
+//    println(reminder)
+//
+////    Use backslash \ symbol when one what to use quotes within a string
+//    println("I love many places but \"Naivasha\" remains the best place I have ever visited.")
+//}
+
+//Booleans
+
+fun main(){
+    val notificationsEnabled: Boolean=false
+
+    println("Are yor phone notification settings enabled? $notificationsEnabled")
 }
