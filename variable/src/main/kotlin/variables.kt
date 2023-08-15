b3ff003448021cfb7470887d3fcef936c002b648
pcodesdev <njugunapm23@gmail.com>
2023-08-15 09:33:04 +0300

    first commit

---
+++ variable/src/main/kotlin/variables.kt
@@ -0,0 +1,26 @@
+//fun main() {
+////    val count: Int = 2
+//    val count:Int=20
+//    println("You have $count unread messages")
+//}
+
+//fun main(){
+//    val readMail:Int=100
+//    val unreadMail:Int=10
+//
+//    println("You have ${readMail + unreadMail} mails in your inbox")
+//    println("You have read $readMail mails.")
+//    println("You have $unreadMail unread mails")
+//    println("${readMail - 50} mails have been deleted.")
+//}
+
+//Reassigning variables
+fun main(){
+//    val is readonly
+//    val cartTotal=0
+//    var is mutable
+    var cartTotal=0
+    cartTotal=20
+
+    println(cartTotal)
+}
